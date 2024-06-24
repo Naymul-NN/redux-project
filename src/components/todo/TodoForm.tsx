9513f2dda79fe9bbadcb743fce0102a4be238ace
Naymul-NN <naymulislam108@gmail.com>
2024-06-24 23:48:04 +0600

    context api

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -1,18 +1,32 @@
-import React, { useContext } from 'react';
+import React, { FormEvent, useContext, useState } from 'react';
 import { TodoContext } from '../../context/TodoProvider'; // Adjust the import path as necessary
 
 const TodoForm: React.FC = () => {
-    const { state } = useContext(TodoContext);
+    const {state, dispatch } = useContext(TodoContext);
     
-    console.log(state);
+    const [task , setTask] = useState('');
+    
+ console.log(state);
+
+    const handleSubmit = (e: FormEvent) => {
+          e.preventDefault();
+
+          const todo = {
+            id: Math.random().toString(36).substring(2,7),
+            title: task,
+            isCompleated: false,
+
+          }
+          dispatch({type: 'addTodo',payload: todo});
+    }
 
     return (
         <div>
             <h1>Add todo</h1>
-           <form >
+           <form onSubmit={handleSubmit}>
             <label htmlFor='Todo'> Task</label>
-            <input type="text" name='todo' id ="todo" />
-            <button type='submit'> submit</button>
+            <input className=' border border-purple-700' type="text" name='todo' id ="todo" onBlur={(e)=> setTask(e.target.value)}/>
+            <button className='btn ml-10 btn-primary' type='submit'> submit</button>
            </form>
         </div>
     );
